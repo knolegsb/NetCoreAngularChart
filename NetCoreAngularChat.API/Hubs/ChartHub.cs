448a0809a18580c3e514ba2513b2b493a44fbfc0
seancbs <sean.y@centurybizsolutions.com>
2021-01-02 20:02:07 -0800

    initial commit

---
+++ NetCoreAngularChat.API/Hubs/ChartHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using NetCoreAngularChat.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAngularChat.API.Hubs
+{
+    public class ChartHub : Hub
+    {
+        public async Task BroadcastChartData(List<ChartModel> data) => await Clients.All.SendAsync("Broadcastchartdata", data);
+    }
+}
